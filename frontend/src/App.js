5b908c27030793cf71f7658e251487b8dbc645ef
Maxim Kurepov <californium251@gmail.com>
2023-06-23 15:36:34 +0400

    still trying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
 import Chat from './components/Chat';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
@@ -10,11 +9,9 @@ import SignUp from './components/SignUp';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="/" element={<Chat />} />
-      </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
+      <Route path="/" element={<Chat />} />
     </Routes>
   );
 }
